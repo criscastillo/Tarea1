911345d9c9ef8c283d5ce4c1288f20683aa947c6
Cristóbal Castillo <cjcastil@uc.cl>
2016-03-17 15:06:50 -0300

    bugs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,14 +17,13 @@ public class Main {
   public static void main(String[] args) {
 	
     port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
 	
-    get("/", (req, res) -> "Hello World");
-
     get("/status", (req, res) -> {
             res.status(201);
             return null;
         });
+    
+    get("/", (req, res) -> "Hello World");
     
     post("/validarFirma", (req, res) -> {
     	boolean parametrosValidos = true;
