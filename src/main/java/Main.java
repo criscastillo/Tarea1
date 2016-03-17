7edfad2af273a01224b463253b6c8e4a96d28c04
Cristóbal Castillo <cjcastil@uc.cl>
2016-03-17 14:37:16 -0300

    bugs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+
 import java.sql.*;
 import java.util.HashMap;
 import java.util.ArrayList;
@@ -7,6 +8,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import static spark.Spark.*;
+import spark.template.freemarker.FreeMarkerEngine;
+import spark.ModelAndView;
+import static spark.Spark.get;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
